7180d9674a530e0e48eefdb839b44f017079dcd9
asa <405930331@qq.com>
2015-08-19 14:58:52 +0800

    no message

--- cti/trunk.hpp
+++ cti/trunk.hpp
@@ -8,13 +8,14 @@
 
 #include <boost/timer.hpp>
 #include <boost/thread.hpp>
+#include <boost/timer/timer.hpp>
 
 /**
- * 语音卡通道状态描述
- *
- * @author LYL QQ-331461049
- * @date 2015/07/16 18:28
- */
+* 语音卡通道状态描述
+*
+* @author LYL QQ-331461049
+* @date 2015/07/16 18:28
+*/
 enum trunk_state {
 	TRK_IDLE,	                            // 空闲
 	TRK_WAIT_CONNECT,	                    // 被占用, 等待连接
@@ -25,12 +26,12 @@ enum trunk_state {
 };
 
 /**
- * 每个语音卡， 都会有 （中继线数量 * 30） 个通道， 每次呼叫都会占用一个通道
- * 此类保存每次呼叫时， 语音卡通道的相关信息
- *
- * @author LYL QQ-331461049
- * @date 2015/07/16 18:35
- */
+* 每个语音卡， 都会有 （中继线数量 * 30） 个通道， 每次呼叫都会占用一个通道
+* 此类保存每次呼叫时， 语音卡通道的相关信息
+*
+* @author LYL QQ-331461049
+* @date 2015/07/16 18:35
+*/
 class trunk
 {
 public:
@@ -39,7 +40,8 @@ public:
 	std::string                    m_called_id;	           // 被叫号码
 	std::string                    m_transId;              // 业务流水号
 	//TODO 此为V1版本boost timer库， 考虑改为V2版本
-	boost::timer                   m_callTime;	           // 发起呼叫的时间
+	//已更改
+	boost::timer::cpu_timer        m_callTime;	           // 发起呼叫的时间
 	bool                           m_hungup_by_echo_tone;  // 是否响一声挂机, false 非响一声挂机情况仅限于测试用, 生产环境设置为 true, 保证响一声立即挂机
 	boost::mutex                   m_trunk_mutex;          // 通道状态锁
 	boost::shared_ptr<base_client> m_client_socket;	       // 每次呼叫时，需要配合其他组件保存的信息
@@ -50,26 +52,26 @@ public:
 	}
 
 	/**
-	 * \brief 获取通道被占用的时间
-	 *
-	 * \return 返回 获取通道被占用的时间， 单位：毫秒
-	 */
+	* \brief 获取通道被占用的时间
+	*
+	* \return 返回 获取通道被占用的时间， 单位：毫秒
+	*/
 	int elpased()
 	{
-		return (int)(m_callTime.elapsed() * 1000);
+		return (int)(m_callTime.elapsed().wall / 1000000);
 	}
 
 	/**
-	 * 每次呼叫完毕， 需要重置相关资源
-	 *
-	 */
+	* 每次呼叫完毕， 需要重置相关资源
+	*
+	*/
 	void realseTrunk()
 	{
 		m_step = TRK_IDLE;
 		m_caller_id.clear();
 		m_called_id.clear();
 		m_transId.clear();
-		m_callTime.restart();
+		m_callTime.start();
 		m_hungup_by_echo_tone = true;
 		m_client_socket.reset();
 	}
